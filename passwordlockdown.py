3033c92346ce96932a2e076475fe6d3ffb545eb7
DEK593 <dek.keed023@gmail.com>
2026-08-20 19:59:30 +0200

    feat: add login security script with lockdown mode

---
+++ passwordlockdown.py
@@ -0,0 +1,56 @@
+import time
+
+admin = "admin"
+root = "root"
+tempo_blocco = 5
+violazioni = []
+esc = "esc"
+
+while True:
+    password_inserita = input("insert user: ")
+    
+    if password_inserita == admin or password_inserita == root:
+        print("accesso negato")
+        violazioni.append(password_inserita)
+
+        if len(violazioni) > 3:
+            print("lockdown activated")
+            
+            while True:
+                time.sleep(tempo_blocco)
+                tentativo = input("insert user2: ")
+                
+                if tentativo == esc:
+                    print("uscita consentita")
+                    violazioni = []
+                    break
+                elif tentativo == root or tentativo == admin:
+                    print("accesso non consentito")
+                    time.sleep(tempo_blocco)
+                else:
+                    print("negato")
+                    time.sleep(50)
+                    tempo_blocco *= 3
+
+    if password_inserita == esc:
+        print("Uscita eseguita.")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
