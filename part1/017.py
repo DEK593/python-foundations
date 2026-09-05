03855945681576f0761176e4bbb971c535782e90
DEK593 <dek.keed023@gmail.com>
2026-09-05 23:38:54 +0200

    day 2

---
+++ part1/017.py
@@ -0,0 +1,7 @@
+num1 = int(input("Number1:"))
+num2 = int(input("Number2:"))
+sum = num1 + num2
+print(f"The sum of the numbers: {sum}")
+
+product = num1  * num2
+print(f"The product of the numbers: {product}")
