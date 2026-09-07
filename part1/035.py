8f3bb4245e2c292dcc498c280e06e2784620c58c
DEK593 <dek.keed023@gmail.com>
2026-09-08 00:20:42 +0200

    day4

---
+++ part1/035.py
@@ -0,0 +1,18 @@
+print("Person 1:")
+
+name = input("Name:")
+
+age = int(input("age:"))
+
+print("Person 2:")
+
+name2 = input("Name:")
+
+age2 = int(input("age:"))
+
+if age > age2:
+    print(f"The elder is {name}")
+elif age2 > age:
+    print(f"The elder is {name2}")
+else:
+    print(f"{name} and {name2} are the same age")
