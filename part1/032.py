8389ff7322b4e673ef7d65fbcba1beda127a329f
DEK593 <dek.keed023@gmail.com>
2026-09-08 20:50:23 +0200

    removed file day 4

--- part1/032.py
+++
@@ -1,4 +0,0 @@
-temperature = float(input("Please type in a number:"))
-
-print(f"Integer part: {int(temperature)}")
-print(f"decimal part: {temperature - int(temperature):.2f}")
--- part1/034.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Please type in the first number:"))
-num2 = int(input("Please type in the second number:"))
-
-if num1 > num2:
-    print(f"the greater number was: {num1}")
-elif num2 > num1:
-    print(f"the greater number was: {num2}")
-else:
-    print("The numbers are equal")
