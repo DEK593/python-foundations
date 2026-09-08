4124e87556412d2f40b9e53438880bc987a0e4c3
DEK593 <dek.keed023@gmail.com>
2026-09-08 20:49:01 +0200

    day 4

---
+++ part2/030.py
@@ -0,0 +1,13 @@
+# Fix the program
+number = int(input("Please type in a number: "))
+
+if number > 100:
+    number -= 100
+    print("The number was greater than one hundred")
+    print("Now its value has decreased by one hundred")
+    print(f"Its value is now {number}")
+    print(f"{number} must be my lucky number!")
+    print("Have a nice day!")
+elif number <= 100:
+    print(f"{number} must be my lucky number!")
+    print("Have a nice day!")
---
+++ part2/032.py
@@ -0,0 +1,4 @@
+temperature = float(input("Please type in a number:"))
+
+print(f"Integer part: {int(temperature)}")
+print(f"decimal part: {temperature - int(temperature):.2f}")
